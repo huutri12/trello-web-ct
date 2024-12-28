e1e8069546208e6eb2c603da3db78c2a99707cbd
huutri12 <tri_1951220112@dau.edu.vn>
2024-12-28 23:33:53 +0700

    Add_dark_light_p1

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,20 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
-// Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    primary: {
-      main: '#556cd6'
+// Create a theme instance.const theme = extendTheme({
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary:teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        primary:cyan,
+        secondary: orange
+      }
     }
   }
 })
