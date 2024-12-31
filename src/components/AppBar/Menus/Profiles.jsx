d1edad5898008a18e28694bc65b01cb0d77eb175
huutri12 <tri_1951220112@dau.edu.vn>
2024-12-31 14:01:11 +0700

    refaactor_theme_color

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -32,7 +32,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 34, height: 34 }}
+            sx={{ width: 36, height: 36 }}
             alt="HuuTriDev"
             src="https://i.pinimg.com/564x/dd/2d/0a/dd2d0a59ad7e79453110b2968af72d89.jpg"
           />
